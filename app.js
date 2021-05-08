7b7a1df3fe4f9c7849e234272dd14c0b52e9a2c9
Edgar Hernandez <EdgarHomeFolder@Edgars-MacBook-Pro.local>
2021-05-08 13:03:44 -0500

    score code

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log("Hello World")
+let test1 = document.getElementById("test1");
+let test2 = document.getElementById("test2");
+let demo = document.getElementById("demo");
+
+function myFunction(){
+    console.log(test1.value);
+    console.log(test2.value);
+    demo1.innerHTML = `<p>Text: ${test1.value}<p>`
+    demo2.innerHTML = `<p>Text 2: ${test2.value}<p>`
+}
+
+let score = 0; 
+
+function scoreKeeper(){
+    if (test1.value == test2.value){
+        score += 10;
+        document.getElementById("score").innerHTML = score;
+        // console.log(score);
+    } else {
+        score += 5;
+        document.getElementById("score").innerHTML = score;
+    }
+}
+
